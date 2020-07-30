e791453922c359499fc24decfc9636ae358f3158
Thet Oo Aung <thetooaung07@gmail.com>
2020-07-30 10:36:18 +0630

    FavouriteList Function added

--- src/components/Favourite.js
+++ src/components/Favourite.js
@@ -4,19 +4,25 @@ import { GlobalContext } from "../Context/GlobalState";
 import { FavouriteList } from "./FavouriteList";
 
 export const Favourite = () => {
+
   const { favourite } = useContext(GlobalContext);
 
   return (
     <div className="Favourite mt-4 w-100">
+      <h3 className="text-left">Favourites</h3>
+      <hr className=" mb-5" />
 
-      <h4 className="text-left">Favourites</h4>
-      <hr></hr>
 
-      { favourite && favourite.length > 0 ? 
-        favourite.map((favourite) => (
-          <FavouriteList key={favourite.id} favourite={favourite}/>
-        )) : <h3 className="text-secondary mt-5">No Movies</h3>
-      }
+
+      <div className="d-flex justify-content-start align-items-center flex-wrap">
+        {favourite && favourite.length > 0 ? (
+          favourite.map((favourite) => (
+            <FavouriteList key={favourite.id} favourite={favourite} />
+          ))
+        ) : (
+          <h3 className="text-secondary">No Movies</h3>
+        )}
+      </div>
     </div>
   );
 };
--- src/components/FavouriteList.js
+++ src/components/FavouriteList.js
@@ -1,28 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../assets/css/FavouriteList.css";
+import { Genre } from "./Genre";
+import { GlobalContext } from "../Context/GlobalState";
 
 
 export const FavouriteList = ({ favourite }) => {
 
-  
+
+  const {removeMovieFromFavourite} = useContext(GlobalContext);
+
 
   return (
-    <div className="FavouriteList mb-3 d-flex justify-content-start align-items-center">
+    <div className="FavouriteList ml-2 mb-3 d-flex justify-content-start align-items-center round-border">
       <div className="mr-2">
         <img
           className="imageResize"
           src={`https://image.tmdb.org/t/p/w92${favourite.poster_path}`}
           alt={favourite.title}
         />
-      </div> 
-      
-      <div className="d-flex flex-column align-items-center">
-        <h5>Heading / Name</h5>
-        <p>Description</p>
       </div>
-      
-      
-      
+
+      <div className="d-flex flex-column">
+        <h5 className="text-left">{favourite.title}</h5>
+        <p className="text-left">
+          Popularity {favourite.popularity.toFixed(2)}
+        </p>
+        <div className="d-flex mb-1">
+        
+          {favourite.genre_ids.map((id) => (
+            <Genre key={id} id={id}></Genre>
+          ))}
+        </div>
+      </div>
+      <button className="btn btn-sm remove-fav" onClick={()=> removeMovieFromFavourite(favourite.id)}><i className="fa fa-times"></i></button>
     </div>
   );
 };
