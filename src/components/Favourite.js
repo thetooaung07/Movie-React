ff270fe35f9ef0909ff0f66c8afe7ae8ed747896
Thet Oo Aung <thetooaung07@gmail.com>
2020-07-30 21:47:30 +0630

    Fixed Fav Ternary Opt with toa branch

--- src/components/Favourite.js
+++ src/components/Favourite.js
@@ -11,39 +11,35 @@ export const Favourite = () => {
     <div className="Favourite mt-4 w-100">
       <h3 className="text-left">Favourites</h3>
       <hr className=" mb-5" />
-      {movieFavourite && movieFavourite.length > 0 &&
-      <div className="">
-        <h5 className="text-left">Favourite Movies</h5>
-        <div className="d-flex justify-content-start align-items-center flex-wrap">
-          {movieFavourite && movieFavourite.length > 0 ? (
-            movieFavourite.map((movieFavourite) => (
-              <FavouriteMovie
-                key={movieFavourite.id}
-                movieFavourite={movieFavourite}
-              />
-            ))
-          ) : (
-            <h3 className="text-secondary">No Movies</h3>
-          )}
+      {movieFavourite && movieFavourite.length > 0 && (
+        <div className="">
+          <h5 className="text-left">Favourite Movies</h5>
+          <div className="d-flex justify-content-start align-items-center flex-wrap">
+            {movieFavourite.map((movieFavourite) => (
+            <FavouriteMovie
+              key={movieFavourite.id}
+              movieFavourite={movieFavourite}
+            />
+            ))}
+          </div>
         </div>
-      </div> }
+      )}
 
       {tvShowFavourite && tvShowFavourite.length > 0 && (
         <div className="">
           <h5 className="text-left">Favourite TV-Shows</h5>
           <div className="d-flex justify-content-start align-items-center flex-wrap">
-            {tvShowFavourite && tvShowFavourite.length > 0 ? (
-              tvShowFavourite.map((tvShowFavourite) => (
-                <FavouriteTvShow
-                  key={tvShowFavourite.id}
-                  tvShowFavourite={tvShowFavourite}
-                />
-              ))
-            ) : (
-              <h3 className="text-secondary">No TV Shows</h3>
-            )}
+            {tvShowFavourite.map((tvShowFavourite) => (
+            <FavouriteTvShow
+              key={tvShowFavourite.id}
+              tvShowFavourite={tvShowFavourite}
+            />
+            ))}
           </div>
         </div>
+      )}
+      {movieFavourite.length === 0 && tvShowFavourite.length === 0 && (
+        <h3 className="text-secondary">Empty List</h3>
       )}
     </div>
   );
